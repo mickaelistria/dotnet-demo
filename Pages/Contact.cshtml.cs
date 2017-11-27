4dcd90d95c329e8d91d1e44e3d3b36c2fad4dd03
Mickael Istria <mistria@redhat.com>
2017-11-28 00:07:09 +0100

    Initial commit

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewDotnetProject.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+        	int a = 4;
+        	for (int i = 0; i < a; i++) {
+        		Message += i;
+        	} 
+        	Message += "year";
+            Message += System.DateTime.Now.DayOfWeek;
+        }
+    }
+}
